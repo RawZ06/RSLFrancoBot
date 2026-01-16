16479b93b24d92ad9d63dff4f2fef37ac14d94ca
RawZ06 <longordo.alexandre@gmail.com>
2026-01-16 22:28:41 +0100

    Update rupee salad settings

--- src/main/java/fr/rawz06/rslfrancobot/engine/usecases/salad/customsalad/CustomSaladRupeeUseCase.java
+++ src/main/java/fr/rawz06/rslfrancobot/engine/usecases/salad/customsalad/CustomSaladRupeeUseCase.java
@@ -3,18 +3,25 @@ package fr.rawz06.rslfrancobot.engine.usecases.salad.customsalad;
 import org.springframework.stereotype.Component;
 
 import java.util.Map;
+import java.util.Random;
 
 @Component
 public class CustomSaladRupeeUseCase {
     public void custom(Map<String, Object> settings) {
         settings.put("shopsanity", '4');
-        settings.put("shuffle_pots", "all");
-        settings.put("shuffle_crates", "all");
-        settings.put("shuffle_freestanding_items", "all");
+        String[] keys = {
+                "shuffle_pots",
+                "shuffle_crates",
+                "shuffle_freestanding_items"
+        };
+        String key = keys[new Random().nextInt(keys.length)];
+        settings.put(key, "all");
         settings.put("shuffle_scrubs", "low");
         settings.put("shuffle_beans", true);
         settings.put("shuffle_expensive_merchants", true);
         settings.put("start_with_rupees", true);
         settings.put("correct_potcrate_appearances", "textures_unchecked");
+        settings.put("shuffle_silver_rupees", "regional");
+        settings.put("silver_rupee_pouches_choice", "all");
     }
 }
